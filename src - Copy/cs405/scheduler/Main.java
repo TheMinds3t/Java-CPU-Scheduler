c710dd4af904f0c31ebc715e0c3882a810130f2b
TheMinds3t <a.j.schultz@outlook.com>
2021-10-13 23:35:19 -0500

    merged with io-queue content, added export process log option
    added exportProcessLog functions, button to choose filename and file extension (between .txt and .html)

---
+++ src - Copy/cs405/scheduler/Main.java
@@ -0,0 +1,14 @@
+package cs405.scheduler;
+
+import cs405.scheduler.gui.CPUFrame;
+
+public class Main {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		CPUFrame frame = new CPUFrame(null);
+		frame.setVisible(true);
+	}
+}
